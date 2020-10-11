a32c1b776dcfd778def90fcbe2dea2d5b00aa6e4
superhackerboy <mark@superhackerman.com>
2020-10-11 14:55:09 -0400

    removed extra reducers

--- src/reducers/letters.js
+++
@@ -1,9 +0,0 @@
-const initialState = [];
-
-export default (state = initialState, { type, letter }) => {
-  if (type === "ADD_LETTER") {
-    return [...state, letter];
-  } else if (type === "CLEAR_LETTERS") {
-    return initialState;
-  } else return state;
-};
--- src/reducers/score.js
+++
@@ -1,7 +0,0 @@
-export default (state = null, { type }) => {
-  if (type === "START_GAME") {
-    return 0;
-  } else if (type === "ADD_SCORE") {
-    return state++;
-  } else return state;
-};
