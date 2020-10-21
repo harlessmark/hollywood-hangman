d846b3c05ea1706107b15f2152676cdc49a3bcfd
superhackerboy <mark@superhackerman.com>
2020-10-21 12:37:56 -0400

    made some fonts bolder

--- src/styled/Span.js
+++ src/styled/Span.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export default styled.span`
-  font-weight: bold;
+  -webkit-text-stroke-width: 0.5px;
   color: ${props => (props.score ? "#f25042" : "inherit")};
 `;
