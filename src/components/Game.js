129c5bf685b95b00ad869948363a5c1091ecc195
superhackerboy <mark@superhackerman.com>
2020-10-28 10:24:26 -0400

    movies are now unique each game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,31 @@
 import React, { useEffect } from "react";
 import movie_data from "../data/movie_data.json";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Movie from "./Movie";
 
 const TotallyRandom = require("totally-random");
 
 export default function Game() {
   const dispatch = useDispatch();
+  const { moviesPlayed } = useSelector(state => state.game);
 
   useEffect(() => {
     // gets random movie
     const random = new TotallyRandom();
-    const randomMovie = random.array(movie_data);
+    // const randomMovie = random.array(movie_data);
+    let randomMovie;
+    let notUnique;
+
+    do {
+      randomMovie = random.array(movie_data);
+      notUnique = false;
+
+      for (const moviePlayed of moviesPlayed) {
+        if (moviePlayed.imdbID === randomMovie.imdbID) {
+          notUnique = true;
+        }
+      }
+    } while (notUnique);
 
     // wraps year in () for smoother rendering
     randomMovie.Year = `(${randomMovie.Year})`;
